12b4ed31a83d7cb0a4e83d151917bb697e1bdc50
niltoneapontes <niltoneapontes@gmail.com>
2021-10-05 17:12:22 -0300

    feat: meal details screen

---
+++ lib/components/meal_item.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/utils/app_routes.dart';
+
+class MealItem extends StatelessWidget {
+  final Meal meal;
+
+  const MealItem(this.meal);
+
+  @override
+  Widget build(BuildContext context) {
+    void _selectMeal() {
+      Navigator.of(context).pushNamed(AppRoutes.MEAL_DETAILS, arguments: meal);
+    }
+
+    return InkWell(
+      onTap: _selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        elevation: 4,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16)),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    child: Text(
+                      meal.title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('${meal.duration} min')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(meal.complexityText)
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(meal.costText)
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/meal_details_screen.dart';
 import 'screens/categories_screen.dart';
+import 'screens/categories_meals_screen.dart';
+
+import 'utils/app_routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,16 +13,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-          primarySwatch: Colors.pink,
-          accentColor: Colors.amber,
-          fontFamily: 'Raleway',
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          textTheme: ThemeData.light().textTheme.copyWith(
-                  headline6: TextStyle(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
                 fontSize: 20,
                 fontFamily: 'Roboto',
-              ))),
-      home: CategoriesScreen(),
+              ),
+            ),
+      ),
+      routes: {
+        AppRoutes.HOME: (_) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_VIEWS: (_) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAILS: (_) => MealDetailScreen()
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (_) {
+          return CategoriesScreen();
+        });
+      },
     );
   }
 }
